072f7c54c3ea3fbf06dcea46091f2e3dedb590b9
Bruno Martens <brunomartens@telenet.be>
2020-02-19 16:38:46 +0100

    nedb on angular app

--- apps/analyzer/src/app/app.component.ts
+++ apps/analyzer/src/app/app.component.ts
@@ -18,5 +18,23 @@ export class AppComponent implements OnInit {
     
     ipcRenderer.send('analyzer-ready'); 
     
+    const Datastore = require('nedb');
+        const db = new Datastore({filename: 'c:/Temp/ps/test.db', autoload: true });
+
+        const doc = { hello: 'world'
+                , n: 5
+                , today: new Date()
+                , nedbIsAwesome: true
+                , notthere: null
+                , notToBeSaved: undefined  // Will not be saved
+                , fruits: [ 'apple', 'orange', 'pear' ]
+                , infos: { name: 'nedb' }
+                };
+
+        db.insert(doc, function (err, newDoc) {   // Callback is optional
+        // newDoc is the newly inserted document, including its _id
+        // newDoc has no key called notToBeSaved since its value was undefined
+        const t = 0;
+        });
   }
 }
